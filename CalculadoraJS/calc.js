81b4b57222829c6441864620aa1df051a777ca96
João Lourenço de Camargo Sardinha <150495104+joaocamargoCS@users.noreply.github.com>
2023-12-17 12:50:07 -0300

    Add files via upload

---
+++ CalculadoraJS/calc.js
@@ -0,0 +1,25 @@
+//Declaração de variáveis:
+
+var num1 = parseInt(prompt("Digite o primeiro número"));
+var num2 = parseInt(prompt("Digite o segundo número"));
+
+//Cálculos com as variáveis:
+
+var soma = num1 + num2;
+var subt = num1 - num2;
+var divi = num1 / num2;
+var mult = num1 * num2;
+
+//Prints dos resultados:
+
+alert(
+  "As seguintes operações dos números dados dão respectivamente\nSoma: " +
+    soma +
+    ".\nSubtração: " +
+    subt +
+    ".\nDivisão: " +
+    divi +
+    ".\nMultiplicação: " +
+    mult +
+    "."
+);
