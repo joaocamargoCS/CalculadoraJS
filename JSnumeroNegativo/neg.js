e40742d803457d31f3bde74febcc7916eb28e1e5
João Lourenço de Camargo Sardinha <150495104+joaocamargoCS@users.noreply.github.com>
2023-12-20 17:04:17 -0300

    Add files via upload

---
+++ JSnumeroNegativo/neg.js
@@ -0,0 +1,9 @@
+var num = parseInt(
+  prompt("Digite um número para saber se o mesmo é negativo ou positivo")
+);
+
+if (num < 0) {
+  alert(num + " é um número negativo");
+} else if (num > 0) {
+  alert(num + " é um número positivo");
+}
