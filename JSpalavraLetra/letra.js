c8d728ca1bac9ed462eb4cdc17f2fb04fc61e6f1
João Lourenço <jlcsardinha@gmail.com>
2023-12-26 15:25:01 -0300

    Estudo 26/12/23

---
+++ JSpalavraLetra/letra.js
@@ -0,0 +1,6 @@
+var palavra = prompt("Digite uma palavra: ");
+var contador = 0;
+
+for (contador = 0; contador < palavra.length; contador++) {
+  alert(palavra[contador]);
+}
---
+++ JSparesFOR/pares.js
@@ -0,0 +1,9 @@
+var contador = 0;
+
+for (contador = 1; contador <= 50; contador++) {
+  if (contador % 2 == 0) {
+    alert(contador);
+  }
+}
+
+alert("Fim");
